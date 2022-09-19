d799e9e7e37d70f0a5acae5228b7772ab2d93d69
shiyuant <shiyuant@student.unimelb.edu.au>
2022-09-19 16:18:36 +1000

    Modify the variable names and structure of the customer model

--- models/customerModel.js
+++ models/customerModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-var UserSchema = mongoose.Schema({
+var customerSchema = mongoose.Schema({
     nickname: {
         type: String,
         unique: false,
@@ -18,28 +18,31 @@ var UserSchema = mongoose.Schema({
         required: true,
 
     },
+  },
+  {
+      timestamps: { createdAt: "createTime", updatedAt: "updateTime" },
 });
 
-UserSchema.pre("save", async function (next) { //存储数据之前将密码进行进行加密处理
-    const user = this; // {username,password}
-    if (!user.isModified("password")) {
+customerSchema.pre("save", async function (next) { // Hash the password before saving to database
+    const customer = this; // {username,password}
+    if (!customer.isModified("password")) {
       return next()
     }
-    const salt = await bcrypt.genSalt(saltRounds); //使用bcrypt 模块进行数据加密
-    const hash = bcrypt.hashSync(user.password, salt); //hash
-    user.password = hash; //将加密后的数据复制给password
+    const salt = await bcrypt.genSalt(saltRounds); // use bcrypt to encrypt the data
+    const hash = bcrypt.hashSync(user.password, salt); // hash the password
+    customer.password = hash; // save the hash to the password 
     return next()
-  }) ///
+  }) 
 
   UserSchema.methods.validatePassword = function (password, callback) { //在model上挂载密码验证函数
-    const user = this;
+    const customer = this;
     //hash
-    bcrypt.compare(password, user.password, (err, isMatch) => { //通过bcrypt的compare函数进行解密处理
+    bcrypt.compare(password, customer.password, (err, isMatch) => { //通过bcrypt的compare函数进行解密处理
       if (err) return callback(err);
       callback(null, isMatch);
     });
   };
  
 
-const UserModel = mongoose.model("UserModel", UserSchema);
-module.exports = UserModel;
+const CustomerModel = mongoose.model("CustomerModel", CustomerSchema);
+module.exports = CustomerModel;
