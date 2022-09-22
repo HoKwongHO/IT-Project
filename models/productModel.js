b0e0dad7ea0f4042cc9882749c8d9e481a3daf05
jimi-long <79777549+jimi-long@users.noreply.github.com>
2022-09-22 19:03:54 +1000

    staff schema and controller, product schema

--- models/productModel.js
+++ models/productModel.js
@@ -1,34 +1,6 @@
 const mongoose = require("mongoose");
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-const productSchema = mongoose.Schema({
-  productName: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: number,
-    required: true,
-  },
-  productPicture: {
-    data: Buffer,
-    contentType: String,
-    required: true,
-  },
-  productCatogory: {
-    type: String,
-    required: true,
-  },
-  productDescription: {
-    type: String,
-  },
-  createdAt: { type: Date, immutable: true, default: () => Date.now() },
-  updateAt: { type: Date, default: () => Date.now() },
-=======
-=======
->>>>>>> parent of 5c64864 (staff schema and controller, product controller)
+
 
 var DataSchema = mongoose.Schema({
     productName: {
@@ -46,9 +18,7 @@ var DataSchema = mongoose.Schema({
     productDescription: {
         type: String,
     },
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 5c64864 (staff schema and controller, product c
+
 
 
 const DataModel = mongoose.model("DataModel", DataSchema);
